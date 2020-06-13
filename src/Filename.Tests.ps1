f01f7e89ba563e833e6342b497a46207fbfa5221
= <manni.basi@gmail.com>
2020-06-13 18:36:52 +0100

    Added initial logic and tests for filename 'cleanse'

---
+++ src/Filename.Tests.ps1
@@ -0,0 +1,21 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-Formatted-Filename" {
+    It "Clean up filename" {
+        Get-Formatted-Filename("Some file name  1.something") | Should -Be 'Some_file_name_1.something'
+    }
+}
+
+Describe "Get-String-With-Underscores-For-Spaces" {
+    It "Replace spaces with underscores" {
+        Get-String-With-Underscores-For-Spaces("Some random string  1") | Should -Be 'Some_random_string__1'
+    }
+}
+
+Describe "Get-String-With-Collapsed-Spaces" {
+    It "Replace double spaces with a single space" {
+        Get-String-With-Collapsed-Spaces("Some other random string  1") | Should -Be 'Some other random string 1'
+    }
+}
---
+++ src/Filename.ps1
@@ -0,0 +1,14 @@
+﻿function Get-Formatted-Filename($filename) {
+    $tempString = $filename
+    $tempString = Get-String-With-Collapsed-Spaces($tempString)
+    $tempString = Get-String-With-Underscores-For-Spaces($tempString)
+    return $tempString
+}
+
+function Get-String-With-Underscores-For-Spaces($string) {
+    return $string -replace  " ", "_";
+}
+
+function Get-String-With-Collapsed-Spaces($string) {
+    return $string -replace  "  ", " ";
+}
